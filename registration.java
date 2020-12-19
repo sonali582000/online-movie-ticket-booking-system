7fa7e13827269b399819199b6536a46705758db4
sonali582000 <68811418+sonali582000@users.noreply.github.com>
2020-12-19 19:37:34 +0530

    Add files via upload

---
+++ registration.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author ASus
+ */
+public class registration {
+    
+    public static void main(String[] args)
+    {
+        NewJFrame1 log=new NewJFrame1();
+        log.show();
+    }
+    
+}
